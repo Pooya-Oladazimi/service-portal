4eba5760d4956bd36b41758d7445bf3c4ec752b5
pooya.oa <pooya.oladazimi@tib.eu>
2025-01-24 15:27:46 +0100

    build: change config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
-  basePath: "/service-panel"
+  basePath: process.env.base_path,
+  assetPrefix: process.env.base_path + '/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
