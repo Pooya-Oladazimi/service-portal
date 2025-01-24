6a65e51b9322929679f09567ea66bcd3233748b1
pooya.oa <pooya.oladazimi@tib.eu>
2025-01-24 14:49:57 +0100

    build: making static build

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"; Image
+import Image from "next/image";
 
 
 export default function Footer() {
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/service-panel"
 };
 
 export default nextConfig;
