d38ce83dd37c74b37fc7e1fdee4bff3697951b5f
pooya.oa <pooya.oladazimi@tib.eu>
2025-02-04 17:25:00 +0100

    turn people data to json

--- app/ui/about/people.tsx
+++ app/ui/about/people.tsx
@@ -1,81 +1,100 @@
 import Image from "next/image";
 
 
+import { promises as fs } from "fs";
 
-export default function AboutPeople() {
+
+type personData = {
+  imagePath: string,
+  name: string,
+  affiliation: string,
+  orcid: string
+}
+
+type sectionData = {
+  title: string,
+  persons: personData[]
+}
+
+export default async function AboutPeople() {
   const [imgW, imgH] = [150, 150];
+
+  async function readJsonFile(filePath: string) {
+    try {
+      const file = await fs.readFile(process.cwd() + '/app/ui/about/people.json', 'utf8');
+      const data = JSON.parse(file);
+      return data;
+    } catch (e) {
+      console.error("Error reading YAML file:", e);
+      return null;
+    }
+  }
+
+  const peopleJson = await readJsonFile('') as sectionData[];
+  console.log(peopleJson)
+
 
   return (
     <>
       <p className="header-2"><b>Principal Investigators</b></p>
       <div className="grid grid-rows-1">
         <div className="grid md:grid-cols-3 sm:grid-rows-1">
-          <div className="card">
-            <Image src="/rb.jpg" width={imgW} height={imgH} alt="Baum, Roman" className="mx-auto"></Image>
-            <div className="pt-2">
-              <p className="text-lg"><b>Baum, Roman</b></p>
-              <p className="mb-2">ZB MED - Information Centre for Life Sciences, Cologne </p>
-              <a href="https://orcid.org/0000-0001-5246-9351" className="orcid-id-btn" target="_blank">ID</a>
-            </div>
-          </div>
-          <div className="card">
-            <Image src="/nk.png" width={imgW} height={imgH} alt="Karam, Naouel" className="mx-auto"></Image>
-            <div className="pt-2">
-              <p className="text-lg"><b>Karam, Naouel</b></p>
-              <p className="mb-2">Institute for Applied Informatics (InfAI), Leipzig</p>
-              <a href="https://orcid.org/0000-0002-6762-6417" className="orcid-id-btn" target="_blank">ID</a>
-            </div>
-          </div>
-          <div className="card">
-            <Image src="/ok.png" width={imgW} height={imgH} alt="Koepler, Oliver" className="mx-auto"></Image>
-            <div className="pt-2">
-              <p className="text-lg"><b>Koepler, Oliver</b></p>
-              <p className="mb-2">TIB - Leibniz Information Centre for Science and Technology, Hannover</p>
-              <a href="https://orcid.org/0000-0003-3385-4232" className="orcid-id-btn" target="_blank">ID</a>
-            </div>
-          </div>
+          {
+            //@ts-ignore
+            peopleJson['sections'][0]['persons'].map((person: personData) => {
+              return (
+                <div className="card">
+                  <Image src={person.imagePath} width={imgW} height={imgH} alt={person.name} className="mx-auto"></Image>
+                  <div className="pt-2">
+                    <p className="text-lg"><b>{person.name}</b></p>
+                    <p className="mb-2">{person.affiliation}</p>
+                    <a href={person.orcid} className="orcid-id-btn" target="_blank">ID</a>
+                  </div>
+                </div>
+              );
+            })
+          }
         </div>
       </div>
       <p className="header-2"><b>Project Team Members</b></p>
       <div className="grid grid-rows-1">
         <div className="grid grid-cols-3">
-          <div className="card">
-            <Image src="/blank.jpg" width={imgW} height={imgH} alt="Sasse, Julia" className="mx-auto"></Image>
-            <div className="pt-2">
-              <p className="text-lg"><b>Sasse, Julia</b></p>
-              <p className="mb-2">ZB MED - Information Centre for Life Sciences, Cologne </p>
-              <a href="https://orcid.org/0000-0002-0660-7597" className="orcid-id-btn" target="_blank">ID</a>
-            </div>
-          </div>
-          <div className="card">
-            <Image src="/blank.jpg" width={imgW} height={imgH} alt="Bouazzouni, Syphax" className="mx-auto"></Image>
-            <div className="pt-2">
-              <p className="text-lg"><b>Bouazzouni, Syphax</b></p>
-              <p className="mb-2">Institute for Applied Informatics (InfAI), Leipzig</p>
-              <a href="https://orcid.org/0000-0002-2069-8739" className="orcid-id-btn" target="_blank">ID</a>
-            </div>
-          </div>
-          <div className="card">
-            <Image src="/blank.jpg" width={imgW} height={imgH} alt="Oladazimi, Pooya" className="mx-auto"></Image>
-            <div className="pt-2">
-              <p className="text-lg"><b>Oladazimi, Pooya</b></p>
-              <p className="mb-2">TIB - Leibniz Information Centre for Science and Technology, Hannover</p>
-              <a href="https://orcid.org/0000-0002-5117-070X" className="orcid-id-btn" target="_blank">ID</a>
-            </div>
-          </div>
+          {
+            //@ts-ignore
+            peopleJson['sections'][1]['persons'].map((person: personData) => {
+              return (
+                <div className="card">
+                  <Image src={person.imagePath} width={imgW} height={imgH} alt={person.name} className="mx-auto"></Image>
+                  <div className="pt-2">
+                    <p className="text-lg"><b>{person.name}</b></p>
+                    <p className="mb-2">{person.affiliation}</p>
+                    <a href={person.orcid} className="orcid-id-btn" target="_blank">ID</a>
+                  </div>
+                </div>
+              );
+            })
+          }
+
         </div>
       </div>
-      <p className="header-2"><b>Project Team Members</b></p>
+      <p className="header-2"><b>Former Project Team Members</b></p>
       <div className="grid grid-rows-1">
         <div className="grid grid-cols-3">
-          <div className="card">
-            <Image src="/blank.jpg" width={imgW} height={imgH} alt="Fillies, Jan" className="mx-auto"></Image>
-            <div className="pt-2">
-              <p className="text-lg"><b>Fillies, Jan</b></p>
-              <p className="mb-2">Institute for Applied Informatics (InfAI), Leipzig</p>
-              <a href="https://orcid.org/0000-0002-2997-4656" className="orcid-id-btn" target="_blank">ID</a>
-            </div>
-          </div>
+          {
+            //@ts-ignore
+            peopleJson['sections'][2]['persons'].map((person: personData) => {
+              return (
+                <div className="card">
+                  <Image src={person.imagePath} width={imgW} height={imgH} alt={person.name} className="mx-auto"></Image>
+                  <div className="pt-2">
+                    <p className="text-lg"><b>{person.name}</b></p>
+                    <p className="mb-2">{person.affiliation}</p>
+                    <a href={person.orcid} className="orcid-id-btn" target="_blank">ID</a>
+                  </div>
+                </div>
+              );
+            })
+          }
         </div>
       </div>
     </>
