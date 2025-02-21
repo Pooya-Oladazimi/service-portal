f790b9f28f159747ed2547df6539c73890621108
pooya.oa <pooya.oladazimi@tib.eu>
2025-02-21 08:58:26 +0100

    build

--- app/ui/incubators/projects.tsx
+++ app/ui/incubators/projects.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Project, Person, InucbatorsStatusCmpProps } from "./types";
+import { Project, InucbatorsStatusCmpProps } from "./types";
 
 
 export default function IncubatorProjects(props: InucbatorsStatusCmpProps) {
