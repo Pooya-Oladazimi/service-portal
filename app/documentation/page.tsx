d24cc10498a7bccee302d14613a7e0ff6e9fa448
pooya.oa <pooya.oladazimi@tib.eu>
2025-03-06 17:05:55 +0100

    add gateway doc link to documentation

--- app/documentation/page.tsx
+++ app/documentation/page.tsx
@@ -3,7 +3,12 @@ export default function Documentation() {
     <div className="md:col-span-2">
       <p className="header-1">Service documentation</p>
       <p className="text-justify">
-        Here you can read about service portal and API gateway user guid
+        Here you can read about service portal and API gateway user guid.
+      </p>
+      <p className="text-justify">
+        Here you can read about service portal and API gateway user guid.
+        This is the link to our API Gateway documentation:
+        <a href="https://ts4nfdi.github.io/api-gateway/" target="_blank">https://ts4nfdi.github.io/api-gateway/</a>
       </p>
     </div>
   );
