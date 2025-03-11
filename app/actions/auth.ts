cf2fdc66add985b858b2fd0d3d541b88f49017d9
pooya.oa <pooya.oladazimi@tib.eu>
2025-03-11 09:34:34 +0100

    cleaning

---
+++ app/actions/auth.ts
@@ -0,0 +1,49 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+
+export const authOptions = ({
+	providers: [
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				username: { label: "Username", type: "text" },
+				password: { label: "Password", type: "password" }
+			}, async authorize(credentials) {
+				const req = await fetch(process.env.GATEWAY_BASE_URL! as string + "/auth/login", {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify(credentials)
+				});
+
+				const user = await req.json();
+				if (req.ok && user) {
+					return { ...user, token: user?.token, username: user?.username };
+				}
+				return null;
+
+			}
+		})
+	],
+	callbacks: {
+		//@ts-ignore
+		async jwt({ token, user }) {
+			if (user) {
+				token.token = user.token;
+				token.username = user.username;
+			}
+			return token;
+		},
+		//@ts-ignore
+		async session({ session, token }) {
+			session.user.token = token.token as string;
+			session.user.username = token.username as string;
+			return session;
+		}
+	},
+	secret: process.env.NEXTAUTH_SECRET
+});
+
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST }; 
