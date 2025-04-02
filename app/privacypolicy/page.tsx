ebbc97182281b9a5d489144c0b05a48bfb557345
pooya.oa <pooya.oladazimi@tib.eu>
2025-04-02 12:00:26 +0200

    feat: add legal pages

---
+++ app/privacypolicy/page.tsx
@@ -0,0 +1,17 @@
+
+
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="col-span-3 content-panel">
+      <p className="header-2">Privacy Policy</p>
+      <p>
+        The corresponding privacy policy is located on a page outside the TS4NFDI Service Portal web app and is therefore opened in a new tab.
+      </p>
+      <p>
+        Go to the privacy policy at
+        <a href="https://www.zbmed.de/en/privacy-policy" target="_blank">https://www.zbmed.de/en/privacy-policy</a>
+      </p>
+    </div>
+  );
+}
