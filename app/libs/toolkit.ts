ca337ef89043ee33951db6571f43bfb48fe215a5
pooya.oa <pooya.oladazimi@tib.eu>
2025-05-23 12:01:12 +0200

    feat: fetch the publicatons list from zenodo
    issue #51

--- app/libs/toolkit.ts
+++ app/libs/toolkit.ts
@@ -1,6 +1,12 @@
 
-export function getCurrentDate(): string {
-  const time = new Date()
+export function getCurrentDate(date: string | undefined): string {
+  /**
+   * Return current data. if an input is provided, it convert the given date to a human readable format.
+   * */
+  let time = new Date();
+  if (date) {
+    time = new Date(date);
+  }
   const day = String(time.getDate()).padStart(2, "0");
   const month = String(time.getMonth() + 1).padStart(2, "0");
   const year = time.getFullYear();
--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -1,26 +1,57 @@
-import publicationsInfo from './publications.json';
+'use client'
+
+import { useState, useEffect } from "react";
+import { getCurrentDate } from "../libs/toolkit";
+import { Loading } from "../ui/commons/snippets";
 
 type Publication = {
-  citation: string,
   doi: string,
-  type: "presentatoin" | "proposal" | "poster" | "software"
+  doi_url: string,
+  title: string,
+  metadata: {
+    resource_type: {
+      title: string
+    }
+  },
+  created: string
 }
 
 export default function Publications() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [publicationsInfo, setPublicationsInfo] = useState<Publication[]>([]);
+
+
+  useEffect(() => {
+    const publicatons = async () => {
+      try {
+        let publicationsApi = await fetch("https://zenodo.org/api/records?communities=ts4nfdi&sort=publication-desc");
+        let publicationsApiJson = await publicationsApi.json();
+        setPublicationsInfo(publicationsApiJson["hits"]["hits"]);
+        setLoading(false);
+      } catch {
+        setPublicationsInfo([]);
+        setLoading(false);
+      }
+    }
+    publicatons();
+  }, []);
+
+
   return (
     <div className="col-span-3">
+      {loading && <Loading />}
       {
-        (publicationsInfo as Publication[]).map((pub: Publication, i: number) => {
+        (publicationsInfo).map((pub: Publication, i: number) => {
           return (
             <div className='card' key={pub.doi}>
-              <div className='inline' key={"citation"}>{`${i + 1}. ${pub.citation}`}</div>
-              <span className='name-badge' key={"type"}>{pub.type}</span>
+              <a href={pub.doi_url} target="_blank" key={"title"}>{`${i + 1}. ${pub.title}`}</a>
+              <span className='name-badge' key={"type"}>{pub.metadata.resource_type.title}</span>
               <br />
-              <a href={pub.doi} target='_blank' key={"doi"}>{pub.doi}</a>
+              <small key={"created_at"}>{getCurrentDate(pub.created)}</small>
+              <br />
             </div>
           )
         })
-
       }
     </div>
   );
